9676228067a319701e4686ea42d8dcaf28a645fa
storres8 <stevetorres7562@gmail.com>
2018-06-27 15:21:43 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,88 @@
-# your code goes here
+class Person
+
+attr_reader :name, :happiness, :hygiene
+attr_accessor :bank_account
+
+def initialize(name)
+  @name = name
+  @bank_account = 25
+  @happiness = 8
+  @hygiene = 8
+
+end
+
+def happiness=(happiness)
+  if happiness > 10
+    @happiness = 10
+  elsif happiness < 0
+    @happiness = 0
+  else @happiness = happiness
+  end
+end
+
+def hygiene=(hygiene)
+  if hygiene > 10
+    @hygiene = 10
+  elsif hygiene < 0
+    @hygiene = 0
+  else @hygiene = hygiene
+  end
+end
+
+def happy?
+  if happiness > 7
+    true
+  else
+    false
+  end
+end
+
+def clean?
+  if hygiene > 7
+    true
+  else
+    false
+  end
+end
+
+def get_paid (salary)
+  @bank_account += salary
+  "all about the benjamins"
+end
+
+def take_bath
+  self.hygiene+=4
+  "♪ Rub-a-dub just relaxing in the tub ♫"
+end
+
+def work_out
+self.hygiene-=3
+self.happiness+=2
+"♪ another one bites the dust ♫"
+end
+
+def call_friend(friend)
+friend.happiness+=3
+self.happiness+=3
+"Hi #{friend.name}! It's #{self.name}. How are you?"
+end
+
+def start_conversation(person,topic)
+if topic == "politics"
+  person.happiness-=2
+  self.happiness-=2
+  "blah blah partisan blah lobbyist"
+elsif topic == "weather"
+  person.happiness+=1
+  self.happiness+=1
+  "blah blah sun blah rain"
+else "blah blah blah blah blah"
+
+end
+end
+
+
+
+
+
+end
